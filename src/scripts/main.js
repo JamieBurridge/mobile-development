069cdb6357aa04375538e27fe6c58bf3ceb867b8
JamieBurridge <xross_dev@proton.me>
2023-02-16 19:14:58 +0000

    functionality: add slider

---
+++ src/scripts/main.js
@@ -0,0 +1,10 @@
+const volumeSlider = document.querySelector("input");
+
+const changeSliderValue = () => {
+  document.documentElement.style.setProperty(
+    "--slider-position",
+    `${volumeSlider.value}%`
+  );
+};
+
+volumeSlider.addEventListener("input", changeSliderValue);
