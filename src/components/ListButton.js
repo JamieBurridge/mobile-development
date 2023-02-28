eeef111cc6ef9d63e6fc9acbb7a9bb26abc262ae
JamieBurridge <xross_dev@proton.me>
2023-02-28 19:51:31 +0000

    css

--- src/components/ListButton.js
+++ src/components/ListButton.js
@@ -3,8 +3,8 @@ import Button from "./Button.js";
 export default class ListButton extends Button {
   #listButtonData;
 
-  constructor(data) {
-    super();
+  constructor(data, callback) {
+    super(null, callback);
 
     this.#listButtonData = data;
 
