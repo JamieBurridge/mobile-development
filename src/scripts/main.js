27fdabf91350132d03ed2f1d0350b8f097e9bc0c
JamieBurridge <xross_dev@proton.me>
2023-02-28 21:01:27 +0000

    audio

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,32 +1,88 @@
+import AudioPlayer from "../components/AudioPlayer.js";
 import Controller from "../components/Controller.js";
 import Info from "../components/Info.js";
 import Menu from "../components/Menu.js";
 import RangeBar from "../components/RangeBar.js";
 
 let appData;
+let info;
+let controller;
+let volumeBar;
+let menu;
+let fileInput;
+let audioPlayer;
 
 window.onload = async () => {
   const req = await fetch("app_data.json");
   appData = await req.json();
 
   setupLayout();
+  setupAudio();
+};
+
+const setupAudio = () => {
+  audioPlayer = new AudioPlayer((state, error) => {
+    controller.setState(state);
+
+    switch (state) {
+      case "error":
+        info.update({
+          name: audioPlayer.currentTrack.name,
+          type: state,
+          error: error,
+        });
+        break;
+
+      case "play":
+        break;
+
+      default:
+        break;
+    }
+
+    if (state != "error") {
+      info.update(audioPlayer.currentTrack);
+    }
+  });
+
+  audioPlayer.volume = volumeBar.value;
 };
 
 const setupLayout = () => {
-  const info = new Info("#info", (value) => {
+  info = new Info("#info", (value) => {
     console.log("info", value);
   });
 
-  const controller = new Controller("#controller", (value) => {
+  controller = new Controller("#controller", (value) => {
     console.log("controller", value);
   });
 
-  const volumeBar = new RangeBar("#volume", (value) => {
+  volumeBar = new RangeBar("#volume", (value) => {
     console.log("volume", value);
   });
 
-  const menu = new Menu("#menu", (value) => {
+  menu = new Menu("#menu", (value) => {
+    switch (value.type) {
+      case "opening":
+        info.close();
+        break;
+      case "music":
+      case "file":
+        menu.close();
+        break;
+      case "open":
+        fileInput.click();
+        break;
+
+      default:
+        break;
+    }
     console.log("menu", value);
   });
   menu.data = appData;
+
+  fileInput = document.querySelector("#file-input");
+  fileInput.onchange = () => {
+    console.log("input changed");
+  };
 };
